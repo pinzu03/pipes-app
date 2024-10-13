016e575c5a262da9be827df2303e6f00bac23b65
David Pineda <davidpinedazuluaga@gmail.com>
2024-10-13 17:27:29 -0500

    Se empieza la aplicacion, creando un sistema de rutas hijas para el modulo de products, y se crean las paginas de este modulo, adicional se crea el menubar y se instala primeNg, para este ultimo tambien se crea un modulo encapsulado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { MenubarModule } from 'primeng/menubar';
+
+
+
+@NgModule({
+  exports: [
+    MenubarModule,
+  ]
+})
+export class PrimeNgModule { }
